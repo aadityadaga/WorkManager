ffa7b7d2b9efe4cb235027241d727612a444c7b2
Aditya Daga <aditya.daga@dishtv.in>
2018-10-29 17:26:19 +0530

    Updated
    Updated

--- app/src/main/java/com/example/adityadaga/workmanager/CustomWorker.java
+++ app/src/main/java/com/example/adityadaga/workmanager/CustomWorker.java
@@ -1,15 +1,7 @@
 package com.example.adityadaga.workmanager;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.ProgressDialog;
-import android.content.ActivityNotFoundException;
-import android.content.Context;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Environment;
 import android.support.annotation.NonNull;
-import android.support.v4.app.NotificationCompat;
 import android.widget.Toast;
 
 import java.io.File;
@@ -23,7 +15,8 @@ import androidx.work.Worker;
 
 public class CustomWorker extends Worker {
 
-    private String saveFilePath;
+    public static String saveFilePath;
+
     private WorkerResult mWorkerresult;
 
 
@@ -35,26 +28,13 @@ public class CustomWorker extends Worker {
     public WorkerResult doWork() {
 
         try {
-       mWorkerresult =   downloadFile("https://www.random.in/Pages/DIY/Download-Soa-Pdf.aspx?year=2018&month=01&smsid=17344166&vcnumber=01517349464","" + getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS));
+            mWorkerresult = downloadFile("https://www.dishtv.in/Pages/DIY/Download-Soa-Pdf.aspx?year=2018&month=01&smsid=17344166&vcnumber=01517349464", "" + getApplicationContext().getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        if(mWorkerresult == WorkerResult.SUCCESS){
-            File file = new File(saveFilePath);
-            Intent target = new Intent(Intent.ACTION_VIEW);
-            target.setDataAndType(Uri.fromFile(file), "application/pdf");
-            target.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-            Intent intent = Intent.createChooser(target, "Open File");
-            try {
-                getApplicationContext().startActivity(intent);
-            } catch (ActivityNotFoundException e) {
-                // Instruct the user to install a PDF reader here, or something
-            }
-        }
+        return mWorkerresult;
 
-        return WorkerResult.SUCCESS;
     }
-
 
 
     private WorkerResult downloadFile(String fileURL, String saveDir) throws IOException {
--- app/src/main/java/com/example/adityadaga/workmanager/MainActivity.java
+++ app/src/main/java/com/example/adityadaga/workmanager/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.adityadaga.workmanager;
 
 import android.app.ProgressDialog;
 import android.arch.lifecycle.Observer;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 import androidx.work.OneTimeWorkRequest;
@@ -51,8 +56,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                 break;
                             case SUCCEEDED:
                                 mProgressDialog.dismiss();
+                                File file = new File(CustomWorker.saveFilePath);
+                                Intent target = new Intent(Intent.ACTION_VIEW);
+                                target.setDataAndType(Uri.fromFile(file), "application/pdf");
+                                target.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                                Intent intent = Intent.createChooser(target, "Open File");
+                                try {
+                                    getApplicationContext().startActivity(intent);
+                                } catch (ActivityNotFoundException e) {
+
+                                }
                                 break;
                             case FAILED:
+                                Toast.makeText(MainActivity.this, "Unable to Download",Toast.LENGTH_SHORT).show();
                                 mProgressDialog.dismiss();
                                 break;
                         }
@@ -73,8 +89,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                                 break;
                             case SUCCEEDED:
                                 mProgressDialog.dismiss();
+                                File file = new File(CustomWorker.saveFilePath);
+                                Intent target = new Intent(Intent.ACTION_VIEW);
+                                target.setDataAndType(Uri.fromFile(file), "application/pdf");
+                                target.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+                                Intent intent = Intent.createChooser(target, "Open File");
+                                try {
+                                    getApplicationContext().startActivity(intent);
+                                } catch (ActivityNotFoundException e) {
+
+                                }
                                 break;
                             case FAILED:
+                                Toast.makeText(MainActivity.this, "Unable to Download",Toast.LENGTH_SHORT).show();
                                 mProgressDialog.dismiss();
                                 break;
                         }
